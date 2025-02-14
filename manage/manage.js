521e548422ae79166480d46d37ba3514c970e6a1
SOMASEKAR17 <somasekarnaidu79@gmail.com>
2025-02-14 21:08:45 +0530

    first commit

---
+++ manage/manage.js
@@ -0,0 +1,6 @@
+let allotments=document.querySelector(".allotments")
+let dropdown=document.querySelector(".dropdown");
+
+allotments.addEventListener("click",()=>{
+ dropdown.classList.toggle("active");
+});
